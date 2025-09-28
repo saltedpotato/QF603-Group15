30d2d4af2c645c568ea6456f0c4e5d75f9adfc3b
Ethelda <47073456+saltedpotato@users.noreply.github.com>
2025-09-28 12:36:47 +0800

    added files to read and visualize data. Also saved data with realized vol

---
+++ data/data_config.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+TICKER = "TLT"
+DATA_START_DATE = "2007-01-01"
+DATA_END_DATE = "2025-08-30"
+VOL_WINDOW_DAYS = 5
+SAVE_CSV = True
+PLOT_DATA = True
